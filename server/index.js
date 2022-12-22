f30653210ef512777c147139f760f6595b621885
aniketsinha5552 <104712880+aniketsinha5552@users.noreply.github.com>
2022-12-22 17:07:39 +0530

    initial

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app= express()
+require('dotenv').config();
+
+const memoryModel = require('./models/memories')
+
+const mongoose = require('mongoose')
+const cors = require ('cors')
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb+srv://aniket:aniket5552@thetraveller.y7qinnp.mongodb.net/thetraveller?retryWrites=true&w=majority",()=>{
+    console.log("DB connected")
+})
+
+
+port = process.env.PORT || 9002
+app.listen( port,()=>{ 
+    console.log(`SERVER RUNNING ON PORT ${port} `) 
+})
+
+app.get('/',(req,res)=>{
+    res.send({mesaage:"The Traveller"})
+})
+
+app.get('/getMemories',(req,res)=>{
+    memoryModel.find({}, (err,result)=>{
+        if(err){
+            res.json(err);
+        } else{
+            res.json(result);
+        }
+    })
+})
+
+app.post('/createMemory',async (req,res)=>{
+    const memory = req.body
+    const newMemory = new memoryModel(memory);
+    await newMemory.save();
+    res.send({message:"Memory Created!"})
+})
+
+app.delete('/delete/:id', (req,res)=>{
+    const id =req.params.id
+    memoryModel.findByIdAndRemove(id).exec()
+    res.send("Item Deleted") 
+})
+
+
+
